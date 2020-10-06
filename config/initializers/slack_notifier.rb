b985ce285575fd41b76150d05f6f683fce17e746
yago santos <yagolcs@msn.com>
2020-10-06 16:45:21 -0300

    Modified pages layouts and styles.

--- config/initializers/slack_notifier.rb
+++ config/initializers/slack_notifier.rb
@@ -1,5 +1,5 @@
 module SlackNotifier
-  NOTIFIER = Slack::Notifier.new "https://hooks.slack.com/services/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" do
+  NOTIFIER = Slack::Notifier.new "#{ENV['SLACK_NOTIFIER_WEEBHOOK']}" do
     defaults channel: "#geral",
              username: "notificação"
   end
